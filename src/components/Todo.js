6b406f47ed91364702f1fa4d34bc7846f5480a06
vbarretoo3 <114249911+vbarretoo3@users.noreply.github.com>
2022-10-06 23:30:09 -0400

    Todo React App

---
+++ src/components/Todo.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import firebase from '../firebase';
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRoundedIcon';
+import CheckCircleOutlineRoundedIcon from '@material-ui/icons/CheckCircleOutlineRoundedIcon';
+import HighlighOffRoundedIcon from '@material-ui/icons/HighlightOffRounded';
+import { motion } from 'framer-motion';
+const Todo = ({ todo }) => {
+    const deleteTodo = () => {
+        const todoRef = firebase.database().ref('Todo').child(todo.id);
+        todoRef.remove()
+    }
+    const completeTodo = () => {
+        const todoRef = firebase.database().ref('Todo').child(todo.id);
+        todoRef.update({
+            complete: !todo.complete,
+        })
+    }
+    return (
+        <>
+            <div className='todo'>
+                <li className='list'>
+                    { todo.complete ?
+                    <CheckCircleRoundedIcon className='icon' onClick={completeTodo} fontSize='large' /> :
+                    <CheckCircleOutlineRoundedIcon className='icon' onClick={completeTodo} fontSize='large' />
+                    }
+                    <motion.div>
+                        <HighlighOffRoundedIcon className='icon' onClick={deleteTodo} fontSize='large'/>
+                    </motion.div>
+                    <h5 className={todo.complete ? 'complete' : 'pending '}>{todo.title}</h5>
+                </li>
+            </div>
+        </>
+    );
+}
+export default Todo;
